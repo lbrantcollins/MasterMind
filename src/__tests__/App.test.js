b8ef6ce546fc043ebef16ab0d50e962ef98f96d2
lbrantcollins <lbrantcollins@gmail.com>
2021-02-21 17:59:49 -0600

    sets up __tests__ directory

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 // import { render, screen } from '@testing-library/react';
 import { shallow } from 'enzyme';
-import App from './App';
+import App from '../App';
 
 test('renders the game board', () => {
   const wrapper = shallow(<App />);
